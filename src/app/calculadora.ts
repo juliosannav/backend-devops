b868356f0d295307381151050e97486979569b4d
julioSantibanez <julio_santibanez@hotmail.com>
2024-11-25 19:16:34 -0300

    avances

--- src/app/calculadora.ts
+++ src/app/calculadora.ts
@@ -3,6 +3,14 @@ function operar(operacion: string = "", a: number, b: number) {
         return suma(a, b);
     } else if (operacion === 'resta') {
         return restar(a, b);
+    }else if (operacion === 'multiplica') {
+        return multiplicar(a, b);
+    }else if (operacion === 'divide') {
+        return dividir(a, b);
+    }else if (operacion === 'potencia') {
+        return potenciar(a, b);
+    }else if (operacion === 'factor') {
+        return factorial(a);
     }
 }
 
@@ -19,7 +27,7 @@ function suma(a: number, b: number) {
 function restar(a: number, b: number) {
     if (a === undefined || b === undefined) {
 
-        throw new Error("No se puede sumar indefinidos");
+        throw new Error("No se puede restar indefinidos");
     }
     if (typeof a !== 'number' || typeof b !== 'number') {
         return NaN;
@@ -27,4 +35,44 @@ function restar(a: number, b: number) {
     return a - b;
 }
 
-export { suma, operar, restar };
+function multiplicar(a: number, b: number) {
+    if (a === undefined || b === undefined) {
+        throw new Error("No se puede multiplicar indefinidos");
+    }
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        return NaN;
+    }
+    return a * b;
+}
+
+function dividir(a: number, b: number) {
+    if (a === undefined || b === undefined) {
+        throw new Error("No se puede dividir indefinidos");
+    }
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        return NaN;
+    }
+    return a / b;
+}
+
+function potenciar(a: number, b: number) {
+    if (a === undefined || b === undefined) {
+        throw new Error("No se puede potenciar indefinidos");
+    }
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        return NaN;
+    }
+    return a ** b;
+}
+
+function factorial(a: number) {
+
+    
+    let result = 1;
+    for (let i = 2; i <= a; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+export { suma, operar, restar ,multiplicar,dividir,potenciar,factorial};
